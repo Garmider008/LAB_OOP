076e31f88997406be259c525e29925395441b984
Garmider008 <60926928+Garmider008@users.noreply.github.com>
2020-09-30 13:01:55 +0300

    Add files via upload

---
+++ Task_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace readline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите переменую типа int");
+            int a = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Введите переменую типа double");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите переменую типа long");
+            long c = long.Parse(Console.ReadLine());
+
+            Console.WriteLine($"a={a};b={b};c={c}");
+        }
+    }
+}
